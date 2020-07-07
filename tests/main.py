2f4ee1d32aaa1f119ade0f12c771d8e34fbcab58
Michal Mitter <mittermichal@gmail.com>
2020-07-07 14:00:37 +0200

    test size match

--- tests/main.py
+++ tests/main.py
@@ -5,3 +5,7 @@ def test_parse_demo_size_from_match_page():
     with open('../cache/ChallengeTV/demos/view/10', 'r') as f:
         size = web.parse_demo_size_from_match_page(f.read())
     assert size['value'] == 1.25 and size['unit'] == 'MB'
+
+
+def test_size_match():
+    assert web.size_match(7727104, 7.37)
